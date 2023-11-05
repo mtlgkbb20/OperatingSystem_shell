38844547bcba969f721925110a6de2a5981f42aa
Mustafa Tolga Akbaba <76005690+mtlgkbb20@users.noreply.github.com>
2023-11-05 17:16:46 +0300

    Update st_shifter.sh
    Son grev de yapld

--- st_shifter.sh
+++ st_shifter.sh
@@ -6,12 +6,27 @@ new_string=""
 for ((i=0;i<${#st_input};i++)); do
 	char="${st_input:i:i+1}"
 	asciichar=$(printf "%d" "'$char\n")
-	sum=$((asciichar+shifting-97))
-	if [ $sum -gt 25 ]; then
-		sum=$((sum % 26))
+	num_digit=${#shifting}
+	if (( num_digit == 1 )); then
+		sum=$((asciichar+shifting-97))
+		if [ $sum -gt 25 ]; then
+			sum=$((sum % 26))
+		fi
+		new_char=$(printf "\\$(printf '%03o' "$((sum+97))")\n")	
+		new_string+="$new_char"
+	elif (( num_digit == 0 )); then
+		echo "write a valid integer"
+		exit 1
+	else (( num_digit >= 2 ))
+		shift_num="${shifting:i:1}"
+		echo "${shift_num}"
+		sum=$((asciichar+shift_num-97))
+		if [ $sum -gt 25 ]; then
+			sum=$((sum % 26))
+		fi
+		new_char=$(printf "\\$(printf '%03o' "$((sum+97))")\n")	
+		new_string+="$new_char"
 	fi
-	new_char=$(printf "\\$(printf '%03o' "$((sum+97))")\n")	
-	new_string+="$new_char"
 done
 
 echo "$new_string"
