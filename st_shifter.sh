9c90c6978816c1384626f3800150165b946372e4
Mustafa Tolga Akbaba <76005690+mtlgkbb20@users.noreply.github.com>
2023-11-07 22:02:29 +0300

    Update st_shifter.sh
    commented

--- st_shifter.sh
+++ st_shifter.sh
@@ -1,12 +1,15 @@
+#takes the input
 st_input="$1"
 shifting="$2"
 
 new_string=""
 
 for ((i=0;i<${#st_input};i++)); do
+	# declares the variables
 	char="${st_input:i:i+1}"
 	asciichar=$(printf "%d" "'$char\n")
 	num_digit=${#shifting}
+ 	#case 1
 	if (( num_digit == 1 )); then
 		sum=$((asciichar+shifting-97))
 		if [ $sum -gt 25 ]; then
@@ -14,9 +17,11 @@ for ((i=0;i<${#st_input};i++)); do
 		fi
 		new_char=$(printf "\\$(printf '%03o' "$((sum+97))")\n")	
 		new_string+="$new_char"
-	elif (( num_digit == 0 )); then
+	# invalid case
+ 	elif (( num_digit == 0 )); then
 		echo "write a valid integer"
 		exit 1
+  	#case 2
 	else (( num_digit >= 2 ))
 		shift_num="${shifting:i:1}"
 		echo "${shift_num}"
