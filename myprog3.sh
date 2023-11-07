b9c0116f56a35f3b85ec510b8bd09850d18f5154
Efe Özgen <efe.ozgennn@gmail.com>
2023-11-07 23:48:06 +0300

    Comment lines added

--- myprog3.sh
+++ myprog3.sh
@@ -1,11 +1,21 @@
+
+# The path provided by the user and if not specified, it defaults to the current directory.
 pathname=${1:-$PWD}
 
-oldest_file=$(find "$pathname" -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d ' ' -f 2)
+# Finds and retrieves a list of files in the specified directory, orders them by time, and identifies the oldest file.
+# Limits the depth to 1, ensuring that the search for the oldest file is restricted to the specified directory only.
+oldest_file=$(find "$pathname" -maxdepth 1 -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d ' ' -f 2)
 
+# If the oldest file is found, the script proceeds.
 if [ -n "$oldest_file" ]; then
-        echo "Do you want to delete $oldest_file? (y/n):"
+        echo "Do you want to delete $oldest_file= (y/n):"
+        
+        # Reads the users response.
         read response
+
+        # Decides whether to delete the file based on the users response.
         if [ "$response" == "y" ]; then
+                # Deletes the oldest file.
                 rm "$oldest_file"
 
                 echo "1 file deleted"
@@ -13,4 +23,3 @@ if [ -n "$oldest_file" ]; then
 else
         echo "No files found to delete."
 fi
-~                                  
--- myprog4.sh
+++ myprog4.sh
@@ -1,16 +1,21 @@
 
+# Checks if the number of command-line arguments is zero. If so, displays the usage and exits with an error status.
 if [ $# -eq 0 ]; then
         echo "Usage $0 <filename>"
         exit 1
 fi
 
+# Assigns the provided filename from the command-line arguments.
 filename="$1"
 
+# Checks if the specified file exists. If not, displays an error message and exits with an error status.
 if [ ! -f "$filename" ]; then
         echo "File not found: $filename"
         exit 1
 fi
 
+# Uses sed to perform in-place replacements in the specified file.
+# Replaces each digit (0-9) with its corresponding textual representation.
 sed -i-e 's/\b0\b/sero/g; s/\b1\b/one/g; s/\b2\b/two/g; s/\b3\b/three/g; s/\b4\b/four/g; s/\b5\b/five/g; s/\b6\b/six/g;s/\b7\b/seven/g; s/\b8\b/eight/g; s/\b9\b/nine/g' "$filename"
 
 echo "Numbers replaced with text in $filename"
